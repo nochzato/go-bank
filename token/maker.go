f89062ec2fbc624aa8c08f01998da1abd8768294
nochzato <nochzato@gmail.com>
2024-08-03 00:15:35 +0300

    implemented jwt and paseto tokens

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// Maker interface is used to manage tokens.
+type Maker interface {
+	// CreateToken creates a new token for a specific username and duration.
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken verifies the token and returns it's payload.
+	VerifyToken(token string) (*Payload, error)
+}

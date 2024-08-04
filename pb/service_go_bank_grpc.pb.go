1f34b414e7ce06d24294de0a02d3e4d4caebda5b
nochzato <nochzato@gmail.com>
2024-08-04 23:27:44 +0300

    added rpc handler to update user

--- pb/service_go_bank_grpc.pb.go
+++ pb/service_go_bank_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion9
 const (
 	GoBank_CreateUser_FullMethodName = "/pb.GoBank/CreateUser"
 	GoBank_LoginUser_FullMethodName  = "/pb.GoBank/LoginUser"
+	GoBank_UpdateUser_FullMethodName = "/pb.GoBank/UpdateUser"
 )
 
 // GoBankClient is the client API for GoBank service.
@@ -29,6 +30,7 @@ const (
 type GoBankClient interface {
 	CreateUser(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error)
 	LoginUser(ctx context.Context, in *LoginUserRequest, opts ...grpc.CallOption) (*LoginUserResponse, error)
+	UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UpdateUserResponse, error)
 }
 
 type goBankClient struct {
@@ -59,12 +61,23 @@ func (c *goBankClient) LoginUser(ctx context.Context, in *LoginUserRequest, opts
 	return out, nil
 }
 
+func (c *goBankClient) UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UpdateUserResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(UpdateUserResponse)
+	err := c.cc.Invoke(ctx, GoBank_UpdateUser_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GoBankServer is the server API for GoBank service.
 // All implementations must embed UnimplementedGoBankServer
 // for forward compatibility.
 type GoBankServer interface {
 	CreateUser(context.Context, *CreateUserRequest) (*CreateUserResponse, error)
 	LoginUser(context.Context, *LoginUserRequest) (*LoginUserResponse, error)
+	UpdateUser(context.Context, *UpdateUserRequest) (*UpdateUserResponse, error)
 	mustEmbedUnimplementedGoBankServer()
 }
 
@@ -80,6 +93,9 @@ func (UnimplementedGoBankServer) CreateUser(context.Context, *CreateUserRequest)
 }
 func (UnimplementedGoBankServer) LoginUser(context.Context, *LoginUserRequest) (*LoginUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LoginUser not implemented")
+}
+func (UnimplementedGoBankServer) UpdateUser(context.Context, *UpdateUserRequest) (*UpdateUserResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateUser not implemented")
 }
 func (UnimplementedGoBankServer) mustEmbedUnimplementedGoBankServer() {}
 func (UnimplementedGoBankServer) testEmbeddedByValue()                {}
@@ -138,6 +154,24 @@ func _GoBank_LoginUser_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _GoBank_UpdateUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GoBankServer).UpdateUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: GoBank_UpdateUser_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GoBankServer).UpdateUser(ctx, req.(*UpdateUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // GoBank_ServiceDesc is the grpc.ServiceDesc for GoBank service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -152,6 +186,10 @@ var GoBank_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LoginUser",
 			Handler:    _GoBank_LoginUser_Handler,
+		},
+		{
+			MethodName: "UpdateUser",
+			Handler:    _GoBank_UpdateUser_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

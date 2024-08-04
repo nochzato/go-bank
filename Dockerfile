9a0a14860a310eb49cf5be84619e5118703e97ef
nochzato <nochzato@gmail.com>
2024-08-04 21:06:42 +0300

    run db migrations on code

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,6 @@ WORKDIR /app
 COPY . .
 
 RUN go build -o main main.go
-RUN apk add curl
-RUN curl -L https://github.com/golang-migrate/migrate/releases/download/v4.17.1/migrate.linux-amd64.tar.gz | tar xvz
 
 # Run stage
 FROM alpine:3.20
@@ -15,12 +13,10 @@ FROM alpine:3.20
 WORKDIR /app
 
 COPY --from=builder /app/main .
-COPY --from=builder /app/migrate ./migrate
-COPY local.env .
+COPY app.env .
 COPY start.sh .
-COPY db/migration ./migration
+COPY db/migration ./db/migration
 
 EXPOSE 8080
 
 CMD [ "/app/main" ]
-ENTRYPOINT [ "/app/start.sh" ]
--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/nochzato/go-bank/api"
@@ -30,9 +33,24 @@ func main() {
 		log.Fatalf("cannot connect to the db: %s", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatalf("cannot create migration: %s", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("failed to run migrate up: %s", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGinServer(config util.Config, store db.Store) {
